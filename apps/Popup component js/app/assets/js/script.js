71322bc84699201c34147988c6eb723eb823cff3
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-27 12:25:56 +0100

    added popup component

---
+++ apps/Popup component js/app/assets/js/script.js
@@ -0,0 +1,25 @@
+"use:strict";
+
+/****************************************
+ *            <code></code>
+ * 
+ *****************************************/
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('.popup-close');
+
+
+
+
+
+button.addEventListener('click', () => {
+  popup.style.display = "block";
+});
+
+close.addEventListener('click', () => {
+  popup.style.display = "none";
+});
+
+popup.addEventListener('click', () => {
+  popup.style.display = "none";
+});
